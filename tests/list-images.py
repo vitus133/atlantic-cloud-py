1f264baa35ea18d15b204b0b034c76af3a5bd0d9
MarcuZ <mz@bemigot.net>
2016-11-21 17:06:30 +0300

    add .image() example

---
+++ tests/list-images.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import atlantic as api
+
+keyfile=os.path.expanduser(sys.argv[1])
+key = {}
+with open(keyfile) as kf:
+    for line in kf:
+        name, val = line.split()
+        key[name.lower()]=val.encode()
+
+me=api.Atlantic(key['public'],key['private'])
+
+def image_as_tsv(i=None):
+    i1 = 'platform'
+    i2 = 'architecture'
+    i3 = 'imageid'
+
+    if i is None:
+        tsv=''.join([
+        i1,'   \t',
+        i2[:4],'\t',
+        i3,
+        ])
+    else:
+        tsv = ''.join([ 
+        i[i1],'   \t',
+        i[i2],'\t',
+        i[i3],
+        ])
+    return tsv
+
+print(image_as_tsv())
+    
+images=me.image.describe()['describe-imageresponse']['imagesset']
+    
+for k,i in images.items():
+    print(image_as_tsv(i))
